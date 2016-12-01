a7ad5fb1b2218b6c849c3ea09a5aac5c059ed6cf
俊哥哥 <jungege@jungegedeMacBook-Air.local>
2016-12-01 16:31:21 +0800

    这就是人生

---
+++ test/test/test/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  test
+//
+//  Created by 俊哥哥 on 2016/12/1.
+//  Copyright © 2016年 俊哥哥. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    
+    
+    NSLog(@"这就是人生");
+    
+}
+
+
+
+
+@end
